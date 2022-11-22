240c9171272b61c91df0a32979e717804d763023
jeffersonjpr <jefferson_michael407@hotmail.com>
2022-11-22 18:47:13 -0300

    simple get learning routes and headers usage

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/hello', function () {
+    return response('Hello World', 200)
+        ->header('Content-Type', 'text/plain')
+        ->header('FOO', 'BAR');
+});
