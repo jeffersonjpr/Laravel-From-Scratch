9a46f395af9b9a3c8886c59bff234a4715554958
jeffersonjpr <jefferson_michael407@hotmail.com>
2022-11-23 16:51:02 -0300

    no listings found

--- resources/views/listings.blade.php
+++ resources/views/listings.blade.php
@@ -1,8 +1,7 @@
 <h1>{{ $heading }}</h1>
-@php
-    $test = 1;
-@endphp
-{{ $test }}
+@if(count($listings) == 0)
+    <p>No listings found</p>
+@endif
 @foreach ($listings as $listing)
     <div class="listing">
         <h2>{{ $listing['title'] }}</h2>
