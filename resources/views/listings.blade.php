cc99f458c06f97942c552e85f17d242a2d8fd8e7
jeffersonjpr <jefferson_michael407@hotmail.com>
2022-11-23 16:49:47 -0300

    blades templates

---
+++ resources/views/listings.blade.php
@@ -0,0 +1,11 @@
+<h1>{{ $heading }}</h1>
+@php
+    $test = 1;
+@endphp
+{{ $test }}
+@foreach ($listings as $listing)
+    <div class="listing">
+        <h2>{{ $listing['title'] }}</h2>
+        <p>{{ $listing['description'] }}</p>
+    </div>
+@endforeach
--- resources/views/listings.php
+++
@@ -1,7 +0,0 @@
-<h1><?php echo $heading; ?></h1>
-<?php foreach($listings as $listing): ?>
-    <div class="listing">
-        <h2><?php echo $listing['title']; ?></h2>
-        <p><?php echo $listing['description']; ?></p>
-    </div>
-<?php endforeach; ?>
