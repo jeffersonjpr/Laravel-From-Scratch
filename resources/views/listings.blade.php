d6d8155ae642a700c6176e41749a9a6b346833ea
jeffersonjpr <jefferson_michael407@hotmail.com>
2022-11-23 16:52:35 -0300

    blades templates

--- resources/views/listings.blade.php
+++ resources/views/listings.blade.php
@@ -1,10 +1,11 @@
 <h1>{{ $heading }}</h1>
-@if(count($listings) == 0)
-    <p>No listings found</p>
-@endif
-@foreach ($listings as $listing)
-    <div class="listing">
-        <h2>{{ $listing['title'] }}</h2>
-        <p>{{ $listing['description'] }}</p>
-    </div>
-@endforeach
+@unless(count($listings) == 0)
+    @foreach ($listings as $listing)
+        <div class="listing">
+            <h2>{{ $listing['title'] }}</h2>
+            <p>{{ $listing['description'] }}</p>
+        </div>
+    @endforeach
+@else
+    <p>No listings found.</p>
+@endunless
