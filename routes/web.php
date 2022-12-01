162eac2c79a33396e239db558a17eeac012b4207
jeffersonjpr <jefferson_michael407@hotmail.com>
2022-12-01 18:43:39 -0300

    Controllers php artisan make:controller ListingController

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,9 @@
 <?php
 
+use App\Http\Controllers\ListingController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use App\Models\Listing;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -15,17 +16,8 @@ use App\Models\Listing;
 |
 */
 
-Route::get('/', function () {
-    return view('listings', [
-        'heading' => 'Lastest Listings',
-        'listings' => Listing::all()
-    ]);
-});
+Route::get('/', [ListingController::class, 'index']);
 
 // Single listing
 
-Route::get('/listings/{listing}', function (Listing $listing) {
-    return view('listing', [
-        'listing' => $listing
-    ]);
-});
+Route::get('/listings/{listing}', [ListingController::class, 'show']);
