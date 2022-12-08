98cde074f0119528de4033abf17f39b0dc43dba3
jeffersonjpr <jefferson_michael407@hotmail.com>
2022-12-08 16:48:36 -0300

    seach filter with db query

--- app/Http/Controllers/ListingController.php
+++ app/Http/Controllers/ListingController.php
@@ -12,7 +12,7 @@ class ListingController extends Controller
     {
         // dd(request('tag'));
         return view('listings.index', [
-            'listings' => Listing::latest()->filter(request(['tag']))->get()
+            'listings' => Listing::latest()->filter(request(['tag', 'search']))->get()
         ]);
     }
 
